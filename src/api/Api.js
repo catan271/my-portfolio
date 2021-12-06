7a65f83bbe0216be76fcba3a68ba456365539678
Catan <catan271@gmail.com>
2021-12-06 12:32:39 +0700

    api

--- src/api/Api.js
+++ src/api/Api.js
@@ -3,25 +3,26 @@ import axios from "axios";
 class Service {
     constructor() {
         this.api = axios.create({
-            baseURL: 'https://asia-southeast1-catan-portfolio.cloudfunctions.net',
+            baseURL: 'https://asia-southeast1-catan-portfolio.cloudfunctions.net/api',
+            // baseURL: 'http://localhost:5001/catan-portfolio/asia-southeast1/api',
             timeout: 10000
         })
     }
 
     getProjects = () => {
-        return this.api.get('get/projects')
+        return this.api.get('projects')
     }
 
     getAchievements = () => {
-        return this.api.get('get/achievements')
+        return this.api.get('achievements')
     }
 
     getSkills = () => {
-        return this.api.get('get/skills')
+        return this.api.get('skills')
     }
 
     sendMessage = (body) => {
-        return this.api.post('send', body)
+        return this.api.post('', body)
     }
 }
 
